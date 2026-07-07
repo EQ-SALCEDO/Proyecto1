673fe29a5f9bef27318b1ee5693d8e4412d46047
Edgar Quintero <quintero_1525@hotmail.com>
2026-07-06 20:20:23 -0600

    primer paso  del proyecto

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <title>Index</title>
+</head>
+<body>
+   
+    <?php include 'menu.php'; ?>
+
+   <div class="container mt-4 text-center">
+     <h1>HSistema de renta inmobiliaria</h1>
+   </div>
+
+</body>
+</html>
