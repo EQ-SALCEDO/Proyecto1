fdf00dd290a4383f7a55fc0670985bf69a405981
Edgar Quintero <quintero_1525@hotmail.com>
2026-07-10 20:42:08 -0600

    agrego formulario cliente

--- menu.php
+++ menu.php
@@ -12,8 +12,8 @@
           </button>
           <ul class="dropdown-menu dropdown-menu-dark">
             <li><a class="dropdown-item" href="formulario.php">formulario</a></li>
-            <li><a class="dropdown-item" href="formulario_categoria.php">Categoria</a></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+            <li><a class="dropdown-item" href="formulario_categoria.php">Formulario Categoria</a></li>
+            <li><a class="dropdown-item" href="formulario_cliente.php">Formulario Cliente</a></li>
           </ul>
         </li>
       </ul>
